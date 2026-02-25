68635a69b1fadbf66590109663cd67ba62dca1a3
Anterres <140460337+Anterres@users.noreply.github.com>
2026-02-25 20:46:11 +0000

    add

---
+++ php/classes/page.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class page{
+
+    function header(){
+        echo "<html>";
+            echo "<head>";
+                echo "<title>test page</title>";
+            echo "</head>";
+            echo "<body>";
+    }
+
+    function footer(){
+            echo "<body>";
+        echo "</html>";
+    }
+
+}
+
+?>
--- php/index.php
+++ php/index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "hello world";
+require_once $_SERVER['DOCUMENT_ROOT'].'/classes/page.class.php';
+
+$page = new page();
+$page->header();
+
+echo "test body<br/>";
+
+$page->footer();
 
 ?>
